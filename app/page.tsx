ea6c10fcd32a76c57838a841ac58303474c88c2f
briuin <dreamingdexiaoxiaohao@hotmail.com>
2024-05-19 02:30:11 +0800

    implement history weather

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "@/utils/axios";
 import { useRef, useState } from "react";
-interface ApiResponse {
+interface Weather {
   coord: {
     lon: number;
     lat: number;
@@ -43,28 +43,39 @@ interface ApiResponse {
   id: number;
   name: string;
   cod: number;
+  timestamp: number;
 }
 
 export default function Home() {
-  const [data, setData] = useState<ApiResponse | null>(null);
+  const [data, setData] = useState<Weather | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [history, setHistory] = useState<Weather[]>([]);
   const cityInputRef = useRef<HTMLInputElement>(null);
   const countryInputRef = useRef<HTMLInputElement>(null);
 
-  const fetchData = async () => {
-    if (!cityInputRef?.current?.value || !countryInputRef?.current?.value) {
+  const fetchWeather = async (city?: string , country?: string) => {
+    const query = {
+      city: city || cityInputRef?.current?.value,
+      country: country || countryInputRef?.current?.value,
+      apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    }
+    if (!query.city || !query.country) {
       return;
     }
+
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.get<ApiResponse>(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityInputRef.current.value},${countryInputRef.current.value}&appid=${
-          process.env.NEXT_PUBLIC_API_KEY
-        }`
+      const response = await axios.get<Weather>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query.city},${query.country}&appid=${query.apiKey}`
       );
-      setData(response.data);
+      const weather = {
+        ...response.data,
+        timestamp: Date.now()
+      };
+      setData(weather);
+      setHistory([...history, weather]);
     } catch (error) {
       setError("An error occurred while fetching data");
       console.error(error);
@@ -75,12 +86,16 @@ export default function Home() {
 
   const clear = () => {
     if (cityInputRef.current) {
-      cityInputRef.current.value = '';
+      cityInputRef.current.value = "";
     }
 
     if (countryInputRef.current) {
-      countryInputRef.current.value = '';
+      countryInputRef.current.value = "";
     }
+  };
+
+  const removeHistory = (timestamp: number) => {
+    setHistory(history.filter((x) => x.timestamp !== timestamp));
   }
 
   return (
@@ -95,16 +110,16 @@ export default function Home() {
         <div className="flex gap-[20px]">
           <div className="flex">
             <div>City: </div>
-            <input ref={cityInputRef}/>
+            <input ref={cityInputRef} />
           </div>
 
           <div className="flex">
             <div>Country: </div>
-            <input ref={countryInputRef}/>
+            <input ref={countryInputRef} />
           </div>
 
           <div className="flex gap-2">
-            <button onClick={fetchData}>search</button>
+            <button onClick={() => fetchWeather()}>search</button>
             <button onClick={clear}>clear</button>
           </div>
         </div>
@@ -115,12 +130,18 @@ export default function Home() {
           </div>
         ) : (
           <div className="flex flex-col">
-            <div>{data.name}, {data.sys.country}</div>
+            <div>
+              {data.name}, {data.sys.country}
+            </div>
             <div>{data.weather[0].main}</div>
             <div>Description: {data.weather[0].description}</div>
-            <div>Tempetature: {data.main.temp_min} ~ {data.main.temp_max}</div>
+            <div>
+              Tempetature: {data.main.temp_min} ~ {data.main.temp_max}
+            </div>
             <div>Humidity: {data.main.humidity}</div>
-            <div>Time: {new Date(Date.now() + data.timezone).toDateString()}</div>
+            <div>
+              Time: {new Date(Date.now() + data.timezone).toDateString()}
+            </div>
           </div>
         )}
       </div>
@@ -130,9 +151,28 @@ export default function Home() {
         <div className="border-b-solid border-b-[1px] w-full border-b-black">
           <h1>Search History</h1>
         </div>
-        <div className="w-full min-h-[300px] flex justify-center items-center">
+        {
+          !history.length ? 
+          <div className="w-full min-h-[300px] flex justify-center items-center">
           No Record
         </div>
+        : 
+        <div>
+          {history.map((x, i) => (
+            <div className="flex justify-between w-full" key={x.id}>
+              <div className="flex">{i + 1}. {x.name}, {x.sys.country} </div>
+              <div className="flex gap-2">
+                <div>{new Date(Date.now() + x.timezone).toDateString()}</div>
+                <div className="flex gap-2">
+                  <button onClick={() => fetchWeather(x.name, x.sys.country)}>search</button>
+                  <button onClick={() => removeHistory(x.timestamp)}>delete</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        }
+        
       </div>
     </div>
   );
