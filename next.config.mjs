3521a408a9422a070aca714e4951b987177915b6
briuin <dreamingdexiaoxiaohao@hotmail.com>
2024-05-20 02:32:52 +0800

    reset next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix: "/Today-s-Weather/",
-  basePath: "/Today-s-Weather",
-  trailingSlash: true,
 };
 
 export default nextConfig;
